7f096a0082031cf43faf024cc57f06e50799a012
TheSupremeBeing <laisitt@gmail.com>
2012-12-06 23:36:00 -0500

    Commit obj-c code

---
+++ objc/SortingAlgorithms/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SortingAlgorithms
+//
+//  Created by Logan on 12/5/12.
+//  Copyright (c) 2012 Logan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ objc/SortingAlgorithms/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SortingAlgorithms
+//
+//  Created by Logan on 12/5/12.
+//  Copyright (c) 2012 Logan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
