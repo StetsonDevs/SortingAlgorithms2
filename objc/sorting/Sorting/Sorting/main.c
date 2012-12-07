28197ca63c3fc141cda473ae7a58c08e56e5bfc5
TheSupremeBeing <laisitt@gmail.com>
2012-12-07 08:00:55 -0500

    Somethign

---
+++ objc/sorting/Sorting/Sorting/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Sorting
+//
+//  Created by Logan on 12/7/12.
+//  Copyright (c) 2012 Logan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
